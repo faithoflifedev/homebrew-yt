bebea5c13a4aadc8c76f973b362ab7c131aebaf2
Chris D <faithoflifedev@gmail.com>
2023-02-06 13:50:32 -0500

    v2.0.6+9

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -1,8 +1,8 @@
 class Yt < Formula
   desc "Native Dart interface to multiple YouTube REST APIs including the Data API, Live Streaming API and Cloud Vision API."
   homepage "https://github.com/faithoflifedev/yt"
-  url "https://api.github.com/repos/faithoflifedev/yt/tarball/v2.0.6+8"
-  sha256 "d2d5ea098d6a65f6b60552e5f6530125c755d1b580e994456876ea44dac29587"
+  url "https://api.github.com/repos/faithoflifedev/yt/tarball/v2.0.6+9"
+  sha256 "01eb133acd108dd4d7c17ecf750f9c12c8445d79bfe580a6bb6593f409feeb1f"
   license "MIT"
   
   depends_on "dart-lang/dart/dart" => :build
@@ -14,6 +14,6 @@ class Yt < Formula
   end
   
   test do
-    assert_match "obs_websocket v2.0.6+8", shell_output("bin/yt version")
+    assert_match "obs_websocket v2.0.6+9", shell_output("bin/yt version")
   end
 end
